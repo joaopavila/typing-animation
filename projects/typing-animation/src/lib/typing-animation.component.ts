b68c2c8606fc516a31b2b7c8852564a65781cda3
joaopavila <joaopaulo_avila@hotmail.com>
2021-11-24 15:44:50 -0300

    feat: remove console.logs and add change detection

--- projects/typing-animation/src/lib/typing-animation.component.ts
+++ projects/typing-animation/src/lib/typing-animation.component.ts
@@ -1,11 +1,17 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'typing-animation',
   templateUrl: './typing-animation.component.html',
   styleUrls: ['./typing-animation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TypingAnimationComponent implements OnInit {
+export class TypingAnimationComponent {
   @HostBinding('style.--text-color') textAndBorderColor: string;
 
   @Input() set textColor(cursorColor: string) {
@@ -14,12 +20,4 @@ export class TypingAnimationComponent implements OnInit {
 
   @Input() cursor: boolean;
   @Input() text: string;
-
-  constructor() {}
-
-  ngOnInit(): void {
-    console.log('cursor - ', this.cursor);
-    console.log('text - ', this.text);
-    console.log('hostBinding - ', this.textAndBorderColor);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TypingAnimationModule } from 'ng-typing-animation';
+import { TypingAnimationModule } from 'projects/typing-animation/src/public-api';
 import { AppComponent } from './app.component';
 
 @NgModule({
